e3f7ebb5f3281617b367493f1c64b4cc2f6dc1b4
oleglatypov <oleg.latypov@avee-health.com>
2017-08-23 08:53:33 -0400

    First Commit v-0.1

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: '',
+        authDomain: '',
+        databaseURL: '',
+        projectId: '',
+        storageBucket: '',
+        messagingSenderId: ''
+    }
+  };
